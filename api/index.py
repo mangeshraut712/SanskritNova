61e850f6e649eb1b84abf109cc0fd766f5d30085
mangeshraut712 <mbr63@drexel.edu>
2026-03-27 00:27:44 +0530

    Remove mangum to test basic FastAPI

--- api/index.py
+++ api/index.py
@@ -11,12 +11,3 @@ def health():
 @app.get("/info")
 def info():
     return {"version": "1.0.0", "name": "SanskritNova API"}
-
-
-handler = None
-try:
-    from mangum import Mangum
-    handler = Mangum(app)
-except Exception as e:
-    import sys
-    print(f"Error: {e}", file=sys.stderr)
